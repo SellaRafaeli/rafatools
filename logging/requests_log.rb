df9ecda2f491bf3680793570c1ed81928b3654f9
Sella Rafaeli <sella@rafaeli.net>
2019-04-18 20:10:56 -0700

    auto-message

--- logging/requests_log.rb
+++ logging/requests_log.rb
@@ -2,6 +2,10 @@ $requests = $mongo.collection('requests') #track http requests
 
 def log_request(data)
   data = data.just(:time_took)
-  data = data.merge({username: cusername, user_id: cuid, path: request_path, params: _params})
+  data = data.merge({user_id: cuid, path: request_path, params: _params})
   $requests.add(data)
+end
+
+get '/refresh' do
+	true
 end
