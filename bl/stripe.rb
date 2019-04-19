e0daeda477e67cfacda09383545b20e9e46a5c00
Sella Rafaeli <sella@rafaeli.net>
2019-04-18 20:29:04 -0700

    auto-message

--- bl/stripe.rb
+++ bl/stripe.rb
@@ -2,5 +2,18 @@ $stripe_notifs = $mongo.collection('stripe_notifs')
 
 post '/stripe_notif' do
 	$stripe_notifs.add(pr)
+
+	# "checkout.session.completed" means success?
+	# see https://stripe.com/docs/payments/checkout/fulfillment#webhooks
+
+	begin
+		user_id = pr[:data]['object']['client_reference_id']
+		if user_id 
+			$users.update_id(user_id, {paid: Time.now}) rescue nil
+		end
+	rescue => e
+		puts e
+	end
+	
 	{msg: 'ok'}	
 end
