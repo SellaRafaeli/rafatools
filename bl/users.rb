44ecbd0b68b82946e660639bb5d800fdef2110e0
Sella Rafaeli <sella@rafaeli.net>
2019-06-01 18:51:30 -0700

    auto-message

--- bl/users.rb
+++ bl/users.rb
@@ -7,7 +7,7 @@ end
 
 post '/me' do
 	redirect_unless_user
-	$users.update_id(cu['_id'],{gmt_offset: pr[:gmt_offset], active: !!pr[:active]})
+	$users.update_id(cu['_id'],{gmt_offset: pr[:gmt_offset], active: !!pr[:active], num: pr[:num]})
 	flash.message = 'Updated settings.'
 	redirect '/me'
 end
